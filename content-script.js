d564fd94c45d190a48f4da95b644320f54693987
Christine Brovkina <christine@whateverism.de>
2024-10-28 12:48:44 +0100

    adjust styles, add links

--- content-script.js
+++ content-script.js
@@ -1,1 +1,22 @@
-console.log(document.head.querySelectorAll("[href]"))
+const elementsHref = document.head.querySelectorAll("[href]")
+	// console.log(elementsHref);
+
+	elementsHref.forEach(element => {
+		const anchor = document.createElement('a');
+		anchor.href = element.href;
+		anchor.target = '_blank';
+		anchor.textContent = '↗';
+		element.appendChild(anchor);
+	});
+
+	const elementsSrc = document.head.querySelectorAll("[src]")
+	console.log(elementsSrc);
+
+	elementsSrc.forEach(element => {
+		const anchor = document.createElement('a');
+		anchor.href = element.src;
+		anchor.target = '_blank';
+		anchor.textContent = '↗';
+		element.appendChild(anchor);
+	});
+
